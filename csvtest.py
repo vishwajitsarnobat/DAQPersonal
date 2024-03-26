d98f629f6b07f2bc7e8429238ccaf041cdd74523
Vishwajit <vishwajit.sarnobat23@spit.ac.in>
2024-03-26 20:27:20 +0530

    fixed store and read functions and added time stamp as first element of the row in csv file

--- csvtest.py
+++ csvtest.py
@@ -1,5 +1,4 @@
 import csv
-import os
 from datetime import datetime
 
 def store():
@@ -11,43 +10,28 @@ def store():
         [0, 0, 0, 1, 1, 0, 1, 1]
     ]
 
-    print(f'Current working directory: {os.getcwd()}')
-
     csv_file = 'output.csv'
 
-    try:
-        
-        with open(csv_file, 'w', newline='') as file:
-            csv_writer = csv.writer(file)
+    with open(csv_file, 'w', newline='') as file:
+        csv_writer = csv.writer(file)
 
-            for row in data:
-                csv_writer.writerow(row)
+        for row in data:
+            row[0] = datetime.now()
+            csv_writer.writerow(row)
 
         print(f'Data has been written to {csv_file}')
-    except Exception as e:
-        print(f'Error: {e}')
         
 
 def read():
     data_2d = []
 
     csv_file = 'output.csv'
-    # Open the CSV file for reading
+
     with open(csv_file, 'r') as file:
-        # Create a CSV reader object
         csv_reader = csv.reader(file)
         
-        # Iterate through each row in the CSV file
         for row in csv_reader:
-            # Convert each 8-bit binary string to a list of bits
-            bits = [int(bit) for byte in row for bit in byte]
-            
-            # Append the list of bits to the 2D array
-            data_2d.append(bits)
+            converted_row = [int(cell) for cell in row]  # Use int() for integers or float() for floats
+            data_2d.append(converted_row)
 
-        # Print the 2D array (optional)
-        for bits in data_2d:
-            print(bits)
-        
-# Execution of functions
-read()
+        print(data_2d)
