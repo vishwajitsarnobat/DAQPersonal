01ad0b2e6c54519887b2592d8aa722b8e65d73db
Vishwajit <vishwajit.sarnobat23@spit.ac.in>
2024-03-29 14:03:38 +0530

    Fixed subprocess and control over programs

--- HostUI/csvtest.py
+++ HostUI/csvtest.py
@@ -1,35 +1,36 @@
 import csv
 from datetime import datetime
 import serial
-from setup import *
 import sys
 import signal
 
 def read():
-    data_2d = []
-
-    csv_file = 'output.csv'
+    dataarr = []
+    csv_file = 'input.csv'
 
     with open(csv_file, 'r') as file:
         csv_reader = csv.reader(file)
+        for data in csv_reader:
+            dataarr.append(data)
 
-        for row in csv_reader:
-            converted_row = [int(cell) for cell in row]  # Use int() for integers or float() for floats
-            data_2d.append(converted_row)
+    print(dataarr)
 
-    print(data_2d)
     
 def signal_handler(sig, frame):
-    print("Stopping the second program...")
+    print("Stopped executing the second program...")
     sys.exit(0)
     
 def store():
+    serial_port = sys.argv[1]
+    baud_rate = sys.argv[2]
+    signal.signal(signal.SIGINT, signal_handler)
     ser = serial.Serial(serial_port, baud_rate)
 
+    csv_file = 'output.csv'
     result_list = []
 
-    with open('output.csv', 'a', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile)
+    with open(csv_file, 'a', newline='') as file:
+        csv_writer = csv.writer(file)
         
         while True:
             current_time = datetime.now()
@@ -37,25 +38,14 @@ def store():
             result_list.append(formatted_time)
             line = ser.readline()
             for byte in line:
-                if byte == ord('0'):
-                    result_list.append(0)
+                if byte == ord('0'): # this checks if byte(which is converted to ASCII automatically) matches with 0s ASCII.
+                    result_list.append(0) # byte will print ASCII value of 0, hence this is needed.
                 elif byte == ord('1'):
                     result_list.append(1)
             
             print(result_list)
-            
             csv_writer.writerow(result_list)
-            
             result_list.clear()
-            
 
-serial_port = sys.argv[1]
-baud_rate = sys.argv[2]
-signal.signal(signal.SIGINT, signal_handler)
-
-try:
-        while True:
-            store()
-except KeyboardInterrupt:
-    print("\nCtrl+C detected. Stopping the second program...")
-    sys.exit(0)
+# executing main programs
+read()
