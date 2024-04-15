53e906e4a102e4c2115f51c7085fcc4693d618b8
Vishwajit <vishwajit.sarnobat23@spit.ac.in>
2024-04-15 19:14:44 +0530

    Organised and cleaned files

---
+++ HostUI/databasetest.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+# Connect to the SQLite database
+conn = sqlite3.connect('daq.db')
+
+# Create a cursor object
+cursor = conn.cursor()
+
+# Execute a DELETE query to delete all data from a table
+# cursor.execute("DELETE FROM PINDATA") 
+# conn.commit()
+
+# Execute a SELECT query to fetch data
+cursor.execute("SELECT * FROM PINDATA")
+
+# Fetch all rows of the result
+rows = cursor.fetchall()
+
+# Iterate over the rows and print or process each row
+for row in rows:
+    print(row)  # Or process the row as needed
+
+# Close the cursor and connection
+cursor.close()
+conn.close()
